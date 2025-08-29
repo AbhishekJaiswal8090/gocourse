409a564efd76057a2a2f18ce515217cbd918bf7e
Abhishek <abhi84514aaa.in@gmail.com>
2025-08-29 23:38:21 +0530

    Implementing interfaces

--- intermidiate/main.go
+++ intermidiate/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // demosntrating closures
 
@@ -12,7 +15,7 @@ func main() {
 	//fmt.Println(sequence())
 
 	//another closure demonsatrating anonymous function
-/*	subTractor := func() func(int) int {
+	/*	subTractor := func() func(int) int {
 		countDown := 99
 		return func(x int) int {
 			countDown -= x
@@ -31,32 +34,32 @@ func main() {
 	//fmt.Println(sumOfDigit(10))
 
 	//pointers in go
-  /*  var ptr *int
+	/*  var ptr *int
 	a := 10
 	ptr =&a
 	fmt.Println(a,ptr)
 
-	//modifying values of a 
-	*ptr =20 //this is called derefrencing 
+	//modifying values of a
+	*ptr =20 //this is called derefrencing
 	fmt.Println(a)*/
 
 	/*message :="hELLO GO"
 	randMessage :="hrllo \ngo"
-    
+
 	fmt.Println(message)
 	fmt.Println(randMessage)
-*/
+	*/
 
-//struct ing go
+	//struct ing go
 
-//there are two different way of declaring struct variables
-    person :=Person{
+	//there are two different way of declaring struct variables
+	person := Person{
 		firstname: "Abhi",
-		lastname: "Jaiswal",
-		age:32,
-		gender: "male",
+		lastname:  "Jaiswal",
+		age:       32,
+		gender:    "male",
 		address: Address{
-			city: "hetimpur",
+			city:     "hetimpur",
 			district: "deoria",
 		},
 		homecontact: HomePhone{
@@ -65,27 +68,27 @@ func main() {
 		},
 	}
 
-	//fmt.Println(person)
+	fmt.Println(person)
 	//craeting instance of struct
 	var person1 Person
-     person1.firstname="akash"
-	 person1.lastname="jaiswal"
-	 person1.age=32
-	 person1.gender="male"
-	 person1.address.city="kushinagr"
-	 person1.address.district="rampur"
-	 person1.homecontact.cell="23451"
-	 person1.homecontact.home="56730"
+	person1.firstname = "akash"
+	person1.lastname = "jaiswal"
+	person1.age = 32
+	person1.gender = "male"
+	person1.address.city = "kushinagr"
+	person1.address.district = "rampur"
+	person1.homecontact.cell = "23451"
+	person1.homecontact.home = "56730"
 
-	 //fmt.Println(person1)
+	//fmt.Println(person1)
 
-	 //struct method call
-    person.structMethod()
+	//struct method call
+	// person.structMethod()
 
-	//method 
-	r:= Rectangle{
+	//method
+	r := Rectangle{
 		length: 4.4,
-		width :60,
+		width:  60,
 	}
 	r.Scale(10)
 	//fmt.Println(r.Area())
@@ -94,13 +97,43 @@ func main() {
 	//fmt.Println(m.Ispositive())
 
 	//using our interface concepts here
-	c :=Cat{}
+	c := Cat{}
 	ans := makeSound(c)
 	fmt.Println(ans)
 
+	//implementing  another interafce
+	rect := Rectangla{
+		45.8,
+		56.0,
+	}
+	cir := Circle{
+		34.90,
+	}
+	sqr := Square{
+		45.46,
 	}
 
-//a function that demonstartes tha closures
+	rectArea := measureArea(rect)
+	fmt.Println(rectArea)
+
+	rectPeri := measurePerimeter(rect)
+	fmt.Println(rectPeri)
+
+	cirArea := measureArea(cir)
+	fmt.Println(cirArea)
+
+	cirPeri := measurePerimeter(cir)
+	fmt.Println(cirPeri)
+
+	sqrArea := measureArea(sqr)
+	fmt.Println(sqrArea)
+
+	sqrPeri := measurePerimeter(sqr)
+	fmt.Println(sqrPeri)
+
+}
+
+// a function that demonstartes tha closures
 func adder() func() int {
 	i := 0
 	fmt.Println("previous value of i", i)
@@ -112,7 +145,7 @@ func adder() func() int {
 
 }
 
-//recursion in go
+// recursion in go
 func factorial(x int) int {
 	//base case factorial of 0 is 1
 	if x == 0 {
@@ -123,85 +156,140 @@ func factorial(x int) int {
 
 }
 
-func sumOfDigit(x int)int{
-	sum :=x
-	if x==0{
+func sumOfDigit(x int) int {
+	sum := x
+	if x == 0 {
 		return sum
 	}
 
-	return sum + sumOfDigit(x -1)
+	return sum + sumOfDigit(x-1)
 }
 
-//struct in go 
-type Person struct{
-		firstname string 
-		lastname string 
-		age int16
-		gender string
-		address Address
-		homecontact HomePhone
-	}
-type Address struct{
-	city string 
+// struct in go
+type Person struct {
+	firstname   string
+	lastname    string
+	age         int16
+	gender      string
+	address     Address
+	homecontact HomePhone
+}
+type Address struct {
+	city     string
 	district string
 }
 
-type HomePhone struct{
+type HomePhone struct {
 	home string
 	cell string
 }
-	//defining strutc method 
-func (p Person) structMethod(){
+
+// defining strutc method
+func (p Person) structMethod() {
 	fmt.Println(p.firstname)
 	fmt.Println(p.age)
 }
 
-func (p Person) changeAge(){
+func (p Person) changeAge() {
 	p.age++
 }
 
-//methods in detail
-type Rectangle struct{
+// methods in detail
+type Rectangle struct {
 	length float32
-	width float32
+	width  float32
 }
 
-//method with value receiver 
-func (r Rectangle ) Area()float32{
-	return r.length *r.width
+// method with value receiver
+func (r Rectangle) Area() float32 {
+	return r.length * r.width
 }
 
-//method with pointer receiver
-func(r *Rectangle) Scale(factor float32){
-     r.length *= factor
-	 r.width *=factor
+// method with pointer receiver
+func (r *Rectangle) Scale(factor float32) {
+	r.length *= factor
+	r.width *= factor
 }
-    type Myint int
-	func (m Myint) Ispositive() bool{
-       return m > 0
-	}
-//implementing interface in go
-//interfaces in java is a concept which is widely used for method receviver for diffrent types 
-type AnimalSound interface{
+
+type Myint int
+
+func (m Myint) Ispositive() bool {
+	return m > 0
+}
+
+// implementing interface in go
+// interfaces in java is a concept which is widely used for method receviver for diffrent types
+type AnimalSound interface {
 	Sound() string
-}	
+}
 
 type Cat struct{}
 
-func(c Cat) Sound()string{
+func (c Cat) Sound() string {
 	return "meow - meow"
 }
 
 type Dog struct{}
 
-func(d Dog )Sound()string{
+func (d Dog) Sound() string {
 	return "bhau-bhau"
 }
 
 type Crow struct{}
-func(c Crow) Sound()string{
+
+func (c Crow) Sound() string {
 	return "kaww-kaww"
 }
 func makeSound(a AnimalSound) string {
 	return a.Sound()
+}
+
+// implemeting another interface
+type ShapeMeasure interface {
+	Area() float32
+	Perimeter() float32
+}
+
+type Circle struct {
+	radius float32
+}
+
+func (c Circle) Area() float32 {
+	return math.Pi * c.radius * c.radius
+}
+func (c Circle) Perimeter() float32 {
+	return 2 * math.Pi * c.radius
+}
+
+type Rectangla struct {
+	length float32
+	width  float32
+}
+
+func (r Rectangla) Area() float32 {
+	return r.length * r.width
+}
+
+func (r Rectangla) Perimeter() float32 {
+	return 2 * (r.length + r.width)
+}
+
+type Square struct {
+	side float32
+}
+
+func (s Square) Area() float32 {
+	return 4 * s.side * s.side
+}
+
+func (s Square) Perimeter() float32 {
+	return 6 * s.side
+}
+
+func measureArea(s ShapeMeasure) float32 {
+	return s.Area()
+}
+
+func measurePerimeter(s ShapeMeasure) float32 {
+	return s.Perimeter()
 }
