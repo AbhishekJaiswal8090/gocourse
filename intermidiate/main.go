100f8bc5cc77f1a24fba509c83467fd6763c4676
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-01 15:16:01 +0530

    error package

--- intermidiate/main.go
+++ intermidiate/main.go
@@ -6,98 +6,105 @@ import (
 	"math"
 )
 
-
-
-func main(){
+func main() {
 	/*fmt.Println("Hello!")
-	//starting generics in go
-	hii,bro :=genercs("abhi","fuck")
-	fmt.Println(hii,bro)
-	hello("abhi","brotha","hello","you","hell")
-	
-	b,a := swap("abhi","akash")
-	fmt.Println(b,a)
+		//starting generics in go
+		hii,bro :=genercs("abhi","fuck")
+		fmt.Println(hii,bro)
+		hello("abhi","brotha","hello","you","hell")
 
-	//implementing stack
-	s := Stack[int]{
-		elements: []int{},
+		b,a := swap("abhi","akash")
+		fmt.Println(b,a)
+
+		//implementing stack
+		s := Stack[int]{
+			elements: []int{},
+		}
+
+		s.push(45)
+	    s.push(50)
+		s.printElement()
+		s.pop()
+		s.pop()
+		s.printElement()
+	*/
+
+	//error in go
+
+	//res, err := sqrt(-56)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+//	fmt.Println(res)
+
+	data := []byte{}
+	if err := process(data); err != nil {
+		fmt.Println("error", err)
+		return
 	}
-
-	s.push(45)
-    s.push(50)
-	s.printElement()
-	s.pop()
-	s.pop()
-	s.printElement()
-*/
-
-
-//error in go
-
-res ,err :=sqrt(-56)
-if err != nil{
-	fmt.Println(err)
-	return
-}
-fmt.Println(res)
-
+	fmt.Println(data)
 }
 
-
-
-func genercs[T any](a,b T)(T,T){
-	return a,b
+func genercs[T any](a, b T) (T, T) {
+	return a, b
 }
-func hello[T any](a ...T)bool{
-	for _,val :=range a{
-        fmt.Println(val)
+func hello[T any](a ...T) bool {
+	for _, val := range a {
+		fmt.Println(val)
 	}
 	return true
 }
-func swap[T any](a,b T)(T,T){
-	return b,a
+func swap[T any](a, b T) (T, T) {
+	return b, a
 }
 
-//implementing stack in go
-type Stack[T any] struct{
+// implementing stack in go
+type Stack[T any] struct {
 	elements []T
 }
 
-func(s *Stack[T]) push(element T)bool{
-	s.elements=append(s.elements,element)
+func (s *Stack[T]) push(element T) bool {
+	s.elements = append(s.elements, element)
 	fmt.Println("elements is pushed")
-    return true
+	return true
 }
 
-func (s *Stack [T]) pop()(T, bool){
-	if len(s.elements)==0{
+func (s *Stack[T]) pop() (T, bool) {
+	if len(s.elements) == 0 {
 		var zero T
 		fmt.Println("underflow condition")
-		return zero,false
+		return zero, false
 	}
 	element := s.elements[len(s.elements)-1]
-	s.elements=s.elements[:len(s.elements)-1]
+	s.elements = s.elements[:len(s.elements)-1]
 	fmt.Println("element is popped")
-    return element,true
+	return element, true
 }
 
-func (s *Stack[T]) printElement(){
+func (s *Stack[T]) printElement() {
 	fmt.Println(s.elements)
 }
 
-func(s *Stack[T]) isOkay()string{
-    if len(s.elements)==0{
+func (s *Stack[T]) isOkay() string {
+	if len(s.elements) == 0 {
 		return "none elements are pushed "
 	}
 	fmt.Println(s.elements)
 	return "there are all element"
 }
 
-
-//implementing error
-func sqrt(x float64)(float64,error){
-	if x<0{
-		return 0,errors.New("math : square root of negative number")
+// implementing error
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("math : square root of negative number")
 	}
-	return math.Sqrt(x) ,nil
+	return math.Sqrt(x), nil
+}
+
+func process(data []byte) error {
+	if len(data) == 0 {
+		return errors.New("Error:empty data")
+	}
+	return nil
 }
