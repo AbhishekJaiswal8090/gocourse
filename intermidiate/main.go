15d3e8ef168eb249e7cf2be41c3553584497f846
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-02 22:31:18 +0530

    templates in go

--- intermidiate/main.go
+++ intermidiate/main.go
@@ -1,73 +1,28 @@
-//string function in go
-package main 
+package main
 
-import ("fmt"
-       "strings"
-	    "strconv")
+import (
+	"html/template"
+	"os"
+)
 
-func main(){
-  str := "Golang"
-fmt.Println(str[0:4]) // "Gola"
+//text template
 
+func main() {
+	//temp1 := template.New("example")
 
+	temp1, err := template.New("example").Parse("welcome ,{{.name}}! how are you")
+	if err != nil {
+		panic(err)
+	}
 
-
-fmt.Println(strings.Contains("golang", "go"))   // true
-fmt.Println(strings.HasPrefix("golang", "go"))  // true
-fmt.Println(strings.HasSuffix("golang", "ang")) // true
-
-
-fmt.Println(strings.Index("golang", "lan"))   // 2
-fmt.Println(strings.Count("golang", "g"))     // 2
-
-
-
-
-fmt.Println(strings.Contains("golang", "go"))   // true
-fmt.Println(strings.HasPrefix("golang", "go"))  // true
-fmt.Println(strings.HasSuffix("golang", "ang")) // true
-
-
-fmt.Println(strings.Index("golang", "lan"))   // 2
-fmt.Println(strings.Count("golang", "g"))     // 2
-
-
-str1 := "a,b,c"
-parts := strings.Split(str1, ",")
-fmt.Println(parts) // [a b c]
-
-joined := strings.Join(parts, "-")
-fmt.Println(joined) // a-b-c
-
-
-str2 := "I love Java"
-newStr := strings.Replace(str2, "Java", "Go", 1)
-fmt.Println(newStr) // I love Go
+	data := map[string]interface{}{
+		"name": "john",
+	}
+	err = temp1.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
 
 
 
-fmt.Println(strings.ToUpper("golang")) // GOLANG
-fmt.Println(strings.ToLower("GOLANG")) // golang
-
-
-fmt.Println(strings.TrimSpace("   abhi   ")) // "abhi"
-fmt.Println(strings.Trim("!!!hello!!!", "!")) // "hello"
-
-
-
-
-// String → Int
-i, _ := strconv.Atoi("123")
-fmt.Println(i) // 123
-
-// Int → String
-s := strconv.Itoa(456)
-fmt.Println(s) // "456"
-
-
-//string formatting
-
-
-
-
-}
+} 
