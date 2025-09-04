6769dcf1dc8ede2e82558e47ff51963fd3778229
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-04 22:48:54 +0530

    url parsing

--- intermidiate/main.go
+++ intermidiate/main.go
@@ -2,41 +2,63 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"net/url"
 )
 
-func main(){
-	numstr :="12345"
-	num ,err :=strconv.Atoi(numstr)
-	if err != nil{
+func main() {
+	//url parsing
+	//[Scheme ://][userinfo@]host[:port][/path][?query][#fragment]
+	rawurl := "https://example.com:8080/path?query=param#fragment"
+	parsedurl, err := url.Parse(rawurl)
+	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println("parsed integer",num +1)
 
-	num1 ,err :=strconv.ParseInt(numstr ,10,32)
-	if err!= nil{
-		fmt.Println(err)
-	}
-	fmt.Println(num1)
+	fmt.Println(parsedurl.Scheme, parsedurl.Fragment, parsedurl.Host, parsedurl.Fragment, parsedurl.Port(), parsedurl.RawQuery)
 
-	floatstr :="34.78"
-	floatnum ,err :=strconv.ParseFloat(floatstr,64)
-	if err!= nil{
-		fmt.Println(err)
-	}
-	fmt.Println(floatnum)
 
-	binarystr :="1010"
-	decimal ,err :=strconv.ParseInt(binarystr,2,64)
-	if err!=nil{
-		fmt.Println(err)
+	rawurl1 :="https://example.com/path?name=john&age=30"
+	parserawurl1,err:=url.Parse(rawurl1); if err !=nil{
+		fmt.Println("Error parsing url",err)
+		return
 	}
-	fmt.Println("parsed binary to decimal val",decimal)
+	queryParams :=parserawurl1.Query()
+    fmt.Println("fckk",queryParams)
+	fmt.Println("Name :",queryParams.Get("name"))
+    	fmt.Println("Age :",queryParams.Get("age"))
 
-	hexstr :="ff"
-	decnum ,err:=strconv.ParseInt(hexstr,16,64)
-	if err!=nil{
-		fmt.Println(err)
-	}
-	fmt.Println(decnum)
+		//building url
+
+		basedURL :=&url.URL{
+			Scheme: "https",
+			Host: "example.com",
+			Path: "/path",
+		}
+		query :=basedURL.Query()
+		query.Set("name","John")
+		query.Set("age","30")
+		basedURL.RawQuery=query.Encode()
+
+		fmt.Println("built url",basedURL.String())
+		val :=url.Values{}
+
+		//add some key val pairs to value object
+		val.Add("name","Abhi")
+		val.Add("age","30")
+		val.Add("city","Hetimpur")
+		val.Add("country","INdia")
+
+		//encoded query
+		encodedquery  :=val.Encode()
+
+		fmt.Println(encodedquery)
+
+		//built a url 
+		basedURL, _ = url.Parse("https://example@.com/search")
+		fullurl := basedURL.String() + "?" + encodedquery
+
+		fmt.Println(fullurl)
+
+	
 }
