82ea97424c292d6f0e4e7ee4e099ad27b9fb53ca
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-06 01:23:02 +0530

    hashing

--- intermidiate/main.go
+++ intermidiate/main.go
@@ -1,52 +1,7 @@
-package main
+package main 
 
-//bufio package
-import (
-	"bufio"
-	"fmt"
-	"os"
-	
-)
- func main(){
+import "fmt"
 
-//   reader :=bufio.NewReader(strings.NewReader("hello ? bufio package fvk u\n"))
-//   data := make([]byte,40)
-  
-//   n,err:=  reader.Read(data)
-//   if err !=nil{
-// 	fmt.Println(err)
-// 	return 
-//   }
-//   fmt.Printf("read %d bytes %s",n,data[:n])
-
-//   line ,err:=reader.ReadString('\n')
-//   if  err != nil{
-// 	fmt.Println(err)
-// 	return
-//   }
-//   fmt.Println("read string",line)
-writer :=bufio.NewWriter(os.Stdout)
-// writting bytes slice first
-data :=[]byte("Hello ,bufio package \n")
- n ,err :=writer.Write(data)
- if err !=nil{
-	fmt.Println(err)
-	return
- }
- fmt.Printf("we wrote %d bytes \n",n)
- //flush the buffer to ensure all the data is written to os.stdout
- err = writer.Flush()
- if err !=nil{
-	fmt.Println(err)
- }
-
- Str :="this is a new string"
-  n ,err =writer.WriteString(Str)
-  if err!=nil{
-	fmt.Println(err)
-	return
-  }
-  fmt.Printf("we wrote %d bytes",n)
-  err = writer.Flush()
-  fmt.Println(err)
- }
+func main(){
+	fmt.Println("Hashing")
+}
