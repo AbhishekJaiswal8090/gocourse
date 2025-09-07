8c0c93844aa7406fdfa03257900027d990235264
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-08 00:18:37 +0530

    reading files

--- intermidiate/main.go
+++ intermidiate/main.go
@@ -1,40 +1,51 @@
 package main
-//writting files
-import ("fmt"
-        "os")
 
-func main(){
-     file,err :=os.Create("output.txt")
-	 if err !=nil{
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("Output.txt")
+	if err != nil {
 		fmt.Println(err)
 		return
-	 }
-	 fmt.Println("File created")
-
-	 defer file.Close()
-
-	 data :=[]byte("HIi it's me")
-	 n,err :=file.Write(data)
-	 if err !=nil{
-		fmt.Println(err)
-	 }
-	 fmt.Println("data has been written",n)
-
-	 //appending directly string insead of creating byte type
-	 file ,err =os.Create("String.txt")
-	 if err!=nil{
-		fmt.Println("error creating file",err)
-		return
-	 }
-	 defer file.Close()
-
-	 fmt.Println("File created successfully")
-
-	 m,err :=file.WriteString("shut the fuck off")
-	 if err!=nil{
-		fmt.Println("Error appending file",err)
-	 }
-	 fmt.Println(m,"data has been appended")
-
-	 
 	}
+	defer func() {
+		fmt.Println("closing open file")
+		defer file.Close()
+
+	}()
+
+	fmt.Println("file opened successfully")
+	// /// reading content of the file
+	// data := make([]byte, 1024)
+	// _, err = file.Read(data)
+
+	// if err != nil {
+	// 	fmt.Println("error reading file", err)
+	// 	return
+	// }
+
+	// fmt.Println("file content", string(data))
+   
+	scanner :=bufio.NewScanner(file)
+
+	//read file line by line
+	for scanner.Scan(){
+		line :=scanner.Text()
+		fmt.Println(line)
+	}
+
+	err =scanner.Err()
+	if err!=nil{
+		fmt.Println("Error reading file",err)
+		return 
+	}
+	
+
+    
+	
+}
---
+++ intermidiate/writtingfile.go
@@ -0,0 +1,40 @@
+package main
+//writting files
+import ("fmt"
+        "os")
+
+func writtingFile(){
+     file,err :=os.Create("output.txt")
+	 if err !=nil{
+		fmt.Println(err)
+		return
+	 }
+	 fmt.Println("File created")
+
+	 defer file.Close()
+
+	 data :=[]byte("HIi it's me")
+	 n,err :=file.Write(data)
+	 if err !=nil{
+		fmt.Println(err)
+	 }
+	 fmt.Println("data has been written",n)
+
+	 //appending directly string insead of creating byte type
+	 file ,err =os.Create("String.txt")
+	 if err!=nil{
+		fmt.Println("error creating file",err)
+		return
+	 }
+	 defer file.Close()
+
+	 fmt.Println("File created successfully")
+
+	 m,err :=file.WriteString("shut the fuck off")
+	 if err!=nil{
+		fmt.Println("Error appending file",err)
+	 }
+	 fmt.Println(m,"data has been appended")
+
+	 
+	}
