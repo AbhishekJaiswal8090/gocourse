3517af0a17910dcb7f9e158015d32015f0e073f2
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-11 00:15:14 +0530

    flag concept III

---
+++ intermidiate/dirConcepts.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// directories
+func dirConcept() {
+	// err :=os.Mkdir("subdir",0755)
+	// checkError(err)
+
+	//creating subdir
+	// checkError(os.Mkdir("subdir", 0755))
+
+
+	//removing subdir
+	// os.RemoveAll("subdir")
+
+	// os.WriteFile("subdir",[]byte("Hello brotha"),0755)
+
+	// checkError(os.MkdirAll("subdir1/parent/child1",0755))
+	// checkError(os.MkdirAll("subdir1/parent/child2",0755))
+	// checkError(os.MkdirAll("subdir1/parent/child3",0755))
+    
+	os.WriteFile("subdir/parent/file",[]byte(""),0755)
+
+	res ,err:=os.ReadDir("subdir/parent")
+    checkError(err)
+
+	for _,val:=range res{
+		fmt.Println(val)
+	}
+}
--- intermidiate/main.go
+++ intermidiate/main.go
@@ -1,40 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
 
-// directories
-func main() {
-	// err :=os.Mkdir("subdir",0755)
-	// checkError(err)
+func main(){
+	subcommand :=flag.NewFlagSet("firstSub",flag.ExitOnError)
+	subcommand2:=flag.NewFlagSet("secondSUb",flag.ExitOnError)
 
-	//creating subdir
-	// checkError(os.Mkdir("subdir", 0755))
-
-
-	//removing subdir
-	// os.RemoveAll("subdir")
-
-	// os.WriteFile("subdir",[]byte("Hello brotha"),0755)
-
-	// checkError(os.MkdirAll("subdir1/parent/child1",0755))
-	// checkError(os.MkdirAll("subdir1/parent/child2",0755))
-	// checkError(os.MkdirAll("subdir1/parent/child3",0755))
-    
-	os.WriteFile("subdir/parent/file",[]byte(""),0755)
-
-	res ,err:=os.ReadDir("subdir/parent")
-    checkError(err)
-
-	for _,val:=range res{
-		fmt.Println(val)
-	}
 }
