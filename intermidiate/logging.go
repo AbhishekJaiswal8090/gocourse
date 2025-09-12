6e574b4f2bcb8465e5b4a16a29411504f43e819b
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-12 13:04:08 +0530

    LOGGING IN GO

--- intermidiate/logging.go
+++ intermidiate/logging.go
@@ -3,10 +3,10 @@ package main
 import (
 	"log"
 	"os"
-	"fmt"
+	
 )
 
-func main(){
+func logging(){
 	log.Println("This is a log message")
 	
 
@@ -22,11 +22,24 @@ func main(){
 	warnLogger.Println("This is an warning message")
 	errorLogger.Println("This is an error messaege")
     
-	file,err :=os.OpenFile("logger.txt",os.O_CREATE|os.O_WRONLY,0755)
+	file,err :=os.OpenFile("logger.txt",os.O_CREATE|os.O_WRONLY|os.O_APPEND,0666)
 	if err !=nil{
-		fmt.Println(err)
-		return
+		log.Fatalf("Failed to opened a file")
 	}
+
+	infoLogger1 :=log.New(os.Stdout,"info: ",log.Ldate | log.Ltime |log.Llongfile)
+	warnLogger1 :=log.New(os.Stdout,"WARN :",log.Ldate | log.Ltime |log.Lshortfile)
+	errorLogger1 :=log.New(os.Stdout,"ERROR :",log.Ldate |log.Ltime |log.Lmicroseconds)
+
+	infoLogger1.Println("This is a info message")
+	warnLogger1.Println("This is a warning message")
+	errorLogger1.Println("This is error message")
+
+	defer file.Close()
+
+	debugLogger :=log.New(file,"DEBUG",log.Ldate|log.Ldate|log.Llongfile)
+	debugLogger.Println("This is a debug message")
+	
 
 }
 //custom logging function
