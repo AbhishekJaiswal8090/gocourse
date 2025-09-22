7eaa18925d1a2ff18edb7ba7d7bfc17d05481630
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-22 20:33:16 +0530

    type conversion in go

--- type_conversion.go
+++ type_conversion.go
@@ -1,5 +1,25 @@
-package main 
+package main
 
-func main(){
+import "fmt"
+
+func main() {
+
+	//int into int32 and float
+	var a int =32
+	b :=int32(a)
+	c:=float64(b)
+	// d:=bool(true)
+
+	e:=3.14
+	f:=int(e)
+	fmt.Println(f,c)
+
+
+	//string into byte
+	g:="hello 0 Arigato ghojaimus"
+	h:=[]byte(g)
+	fmt.Println(h)
+
+
 	
 }
