329bec517ad70d377ed3069cd561f90a1448440c
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-24 22:18:08 +0530

    math package in go

---
+++ intermidiate/math.go
@@ -0,0 +1,45 @@
+package main 
+
+import (
+	"fmt"
+	"math"
+)
+func main(){
+fmt.Println("math package in go")
+fmt.Println(math.Pi*23)
+  p := math.Pi
+
+    e := math.E
+
+    ph := math.Phi
+
+    sqrt2 := math.Sqrt2
+
+    sqrtE := math.SqrtE
+
+    sqrtPi := math.SqrtPi
+
+    sqrtPhi := math.SqrtPhi
+
+    ln2 := math.Ln2
+
+    ln10 := math.Ln10
+
+    log2E := math.Log2E
+
+    log10E := math.Log10E
+
+
+
+fmt.Println(math.Abs(-3.14)) // Output: 3.14
+
+
+
+fmt.Println(math.Sqrt(16)) // Output: 4
+
+
+
+
+
+fmt.Println(math.Pow(2, 3)) // Output: 8
+}
