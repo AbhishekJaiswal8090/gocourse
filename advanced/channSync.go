65640134d00de943ed78c69e5f86dcbdb289ded4
Abhishek <abhi84514aaa.in@gmail.com>
2025-09-26 20:59:44 +0530

    channel syncronization

--- advanced/channSync.go
+++ advanced/channSync.go
@@ -24,19 +24,41 @@ import (
 //  }
 
 
-func main(){
-	ch :=make(chan int)
+// func main(){
+// 	ch :=make(chan int)
 
-	go func(){
-		fmt.Println("sending")
-		ch <-9//blocking until the value is received
-		fmt.Println("Sent value")
-		time.Sleep(2*time.Second)
+// 	go func(){
+// 		fmt.Println("sending")
+// 		ch <-9//blocking until the value is received
+// 		fmt.Println("Sent value")
+// 		time.Sleep(2*time.Second)
 		
-	}()
-	value := <-ch
-	fmt.Println("VALUE RECEIVES",value)
+// 	}()
+// 	value := <-ch
+// 	fmt.Println("VALUE RECEIVES",value)
 
-	time.Sleep(3*time.Second)
+// 	time.Sleep(3*time.Second)
 
+// }
+
+func main(){
+	numOfGoroutine:=3
+	done :=make(chan int ,3)
+
+	for i:= range numOfGoroutine{
+		go func(id int){
+          fmt.Printf("Goroutines %d working \n",id)
+		  time.Sleep(time.Second)
+		  done <- 1
+		}(i)
+	}
+	for range numOfGoroutine{
+		<-done //wait for each goroutines to finish
+	}
+
+	fmt.Println("Finished")
 }
+
+// Channel synchronization means using channels as a way for
+//  goroutines to signal each other 
+// and ensure proper ordering or completion.
